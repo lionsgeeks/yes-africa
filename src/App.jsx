59a62f176f852cf223d3546a1df75c2de2c68c2d
ie-orphane <ilyasselyatime0@gmail.com>
2024-09-02 16:47:25 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-gray-900">
+        <h1 className="text-white text-4xl font-bold">Welcome to Africa !</h1>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
